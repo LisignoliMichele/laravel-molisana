c1393406cb4d1de81f3ba10605411e8aafed87c9
Michele Lisignoli <michele@lisignoli.com>
2021-05-10 14:54:58 +0200

    creata struttura principale del progetto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="">
-      {{-- google font --}}
-      <link rel="preconnect" href="https://fonts.gstatic.com">
-      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
-      {{-- /google font --}}
-      <link rel="stylesheet" href="{{asset('css/app.css')}}">
-      <title>LA MOLISANA - MICHELE</title>
-   </head>
-   <body>
-      @include('partials.header')
-      <main>
-         <div class="container">
-            <section class="lunghe">
-               <h2>le lunghe</h2>
-               <div class="cards">
-                  @foreach ($pastalunga as $pasta)
-                     <div class="card">
-                        <img src="{{$pasta['src']}}" alt="{{$pasta["titolo"]}}">
-                     </div>
-                  @endforeach
-               </div>
-            </section>
-            <section class="corte">
-               <h2>le corte</h2>
-               <div class="cards">
-                  @foreach ($pastacorta as $pasta)
-                     <div class="card">
-                        <img src="{{$pasta['src']}}" alt="{{$pasta["titolo"]}}">
-                     </div>
-                  @endforeach
-               </div>
-            </section>
-            <section class="cortisime">
-               <h2>le cortissime</h2>
-               <div class="cards">
-                  @foreach ($pastacortissima as $pasta)
-                     <div class="card">
-                        <img src="{{$pasta['src']}}" alt="{{$pasta["titolo"]}}">
-                     </div>
-                  @endforeach
-               </div>
-            </section>
-         </div>
-      </main>
-      @include('partials.footer')
-   </body>
-</html>
+{{-- importo la struttura --}}
+@extends('layouts.main')
+
+{{-- titolo pagina --}}
+@section('pageTitle')
+    La molisana HOME - Michele
+@endsection
+
+{{-- contenuto homepage --}}
+@section('content')
+<div class="container">
+   <section class="lunghe">
+      <h2>le lunghe</h2>
+      <div class="cards">
+         @foreach ($pastalunga as $pasta)
+            <div class="card">
+               <img src="{{$pasta['src']}}" alt="{{$pasta["titolo"]}}">
+            </div>
+         @endforeach
+      </div>
+   </section>
+   <section class="corte">
+      <h2>le corte</h2>
+      <div class="cards">
+         @foreach ($pastacorta as $pasta)
+            <div class="card">
+               <img src="{{$pasta['src']}}" alt="{{$pasta["titolo"]}}">
+            </div>
+         @endforeach
+      </div>
+   </section>
+   <section class="cortisime">
+      <h2>le cortissime</h2>
+      <div class="cards">
+         @foreach ($pastacortissima as $pasta)
+            <div class="card">
+               <img src="{{$pasta['src']}}" alt="{{$pasta["titolo"]}}">
+            </div>
+         @endforeach
+      </div>
+   </section>
+</div> 
+@endsection
+         
+  
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="">
+      {{-- google font --}}
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+      {{-- /google font --}}
+      <link rel="stylesheet" href="{{asset('css/app.css')}}">
+      {{-- diamo un placeholder anche al titolo: --}}
+      <title>@yield('pageTitle')</title>
+   </head>
+   <body>
+      @include('partials.header')
+      <main> 
+         @yield('content')
+      </main>
+      @include('partials.footer')
+   </body>
+</html>
